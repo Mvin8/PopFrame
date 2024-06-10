bfedb28b53713aaba2f7de6164518f9e956df227
Mvin8 <skitless1229@gmail.com>
2024-06-10 20:20:04 +0300

    karkas 9 region

--- popframe/method/popuation_frame.py
+++ popframe/method/popuation_frame.py
@@ -4,6 +4,8 @@ import geopandas as gpd
 import folium
 import json
 from ..models.region import Town
+import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
 
 from .base_method import BaseMethod
 
@@ -11,26 +13,46 @@ class PopFrame(BaseMethod):
     """Class provides methods for network analysis on geographic data"""
 
     def _initialize_graph(self, towns: gpd.GeoDataFrame):
-
         """Initializes the graph with nodes"""
         G = nx.Graph()
         for idx, row in towns.iterrows():
             G.add_node(idx, pos=(row.geometry.x, row.geometry.y), level=row['level'])
         return G
 
-    def _connect_levels(self, towns: gpd.GeoDataFrame, G: nx.Graph,  from_level: str, to_levels: List[str]):
+    def _connect_levels(self, towns: gpd.GeoDataFrame, G: nx.Graph, from_level: str, to_levels: List[str]):
         """Connects nodes across specified levels"""
         from_towns = towns[towns['level'] == from_level].index
         to_towns = towns[towns['level'].isin(to_levels)].index
 
+        if to_towns.empty:
+            return
+
+        unconnected_from_towns = set(from_towns)
+
         for idx in from_towns:
             distances = self.region.accessibility_matrix.loc[idx, to_towns]
+            if distances.empty:
+                continue
             nearest_idx = distances.idxmin()
 
             if nearest_idx and not G.has_edge(idx, nearest_idx):
                 G.add_edge(idx, nearest_idx, level=from_level)
+                if idx in unconnected_from_towns:
+                    unconnected_from_towns.remove(idx)
 
-    def _connect_same_level(self, towns: gpd.GeoDataFrame, G: nx.Graph , level: str):
+        # Connect remaining unconnected nodes using Euclidean distance
+        if unconnected_from_towns:
+            to_coords = towns.loc[to_towns].geometry.apply(lambda geom: (geom.x, geom.y)).tolist()
+            for idx in unconnected_from_towns:
+                from_coord = (towns.loc[idx].geometry.x, towns.loc[idx].geometry.y)
+                nearest_idx = min(to_towns, key=lambda to_idx: self._euclidean_distance(from_coord, (towns.loc[to_idx].geometry.x, towns.loc[to_idx].geometry.y)))
+                G.add_edge(idx, nearest_idx, level=from_level)
+
+    def _euclidean_distance(self, coord1, coord2):
+        """Calculates Euclidean distance between two coordinates"""
+        return ((coord1[0] - coord2[0]) ** 2 + (coord1[1] - coord2[1]) ** 2) ** 0.5
+
+    def _connect_same_level(self, towns: gpd.GeoDataFrame, G: nx.Graph, level: str):
         """Connects nodes within the same level using Kruskal's algorithm"""
         level_idxs = towns[towns['level'] == level].index
         H = nx.Graph()
@@ -52,35 +74,75 @@ class PopFrame(BaseMethod):
         towns = self.region.get_towns_gdf().to_crs(4326)
         G = self._initialize_graph(towns)
 
-        self._connect_levels( towns, G , 'Пятый уровень', ['Второй уровень', 'Третий уровень', 'Четвертый уровень'])
-        self._connect_levels(towns, G ,'Четвертый уровень', ['Третий уровень', 'Второй уровень'])
-        self._connect_levels(towns, G ,'Третий уровень', ['Второй уровень'])
-        self._connect_same_level(towns, G ,'Второй уровень')
+        levels = [
+            "Малое сельское поселение",
+            "Среднее сельское поселение",
+            "Большое сельское поселение",
+            "Крупное сельское поселение",
+            "Малый город",
+            "Средний город",
+            "Большой город",
+            "Крупный город",
+            "Крупнейший город",
+            "Сверхкрупный город"
+        ]
 
-        return  G
+        for i, level in enumerate(levels[:-1]):
+            from_level = levels[i]
+            to_levels = levels[i + 1:]
+            self._connect_levels(towns, G, from_level, to_levels)
+
+        # for level in levels:
+        #     self._connect_same_level(towns, G, level)
+
+        return G
+
+
+    def get_color_map(self, levels):
+        # Define the base colors for the heatmap starting from yellow to red
+        base_colors = ['#FFC100', '#FF6500', '#C40C0C', '#6C0345']
+        
+        # Create a colormap
+        cmap = mcolors.LinearSegmentedColormap.from_list("heatmap", base_colors, N=len(levels))
+        
+        # Normalize the color map
+        norm = mcolors.Normalize(vmin=0, vmax=len(levels) - 1)
+        
+        # Generate the colors for each level
+        colors = [mcolors.to_hex(cmap(norm(i))) for i in range(len(levels))]
+        
+        return colors
 
     def get_graph_html(self, G: nx.Graph, filepath: str):
         """Saves the graph as an HTML file with a map visualization"""
         towns = self.region.get_towns_gdf()
+        
+        # Define levels
+        levels = [
+            "Малое сельское поселение",
+            "Среднее сельское поселение",
+            "Большое сельское поселение",
+            "Крупное сельское поселение",
+            "Малый город",
+            "Средний город",
+            "Большой город",
+            "Крупный город",
+            "Крупнейший город",
+            "Сверхкрупный город"
+        ]
+        
+        # Get the colors for each level
+        level_colors = dict(zip(levels, self.get_color_map(levels)))
 
         m = folium.Map(zoom_start=3, tiles='CartoDB Positron')
 
-        level_colors = {
-            'Пятый уровень': '#6996B3',  # Синий для 1 уровня
-            'Четвертый уровень': '#89AC76',  # Зеленый для 2 уровня
-            'Третий уровень': '#FFC107',  # Красный для 3 уровня
-            'Второй уровень': '#B71C1C'  # Желтый для 4 уровня
-        }
-
-
-        # Добавление рёбер графа на карту с логикой цвета из изначального кода
+        # Add edges to the map
         for edge in G.edges(data=True):
             start_pos = G.nodes[edge[0]]['pos']
             end_pos = G.nodes[edge[1]]['pos']
             start_level = G.nodes[edge[0]]['level']
             end_level = G.nodes[edge[1]]['level']
             
-
             lower_level = min(start_level, end_level, key=lambda x: list(level_colors.keys()).index(x))
             edge_color = level_colors[lower_level]
 
@@ -90,47 +152,37 @@ class PopFrame(BaseMethod):
                 weight=2
             ).add_to(m)
 
-        # Добавление узлов графа на карту
+        # Add nodes to the map
         for node, data in G.nodes(data=True):
             pos = data['pos']
             name = towns.loc[node, 'name']
-            # types = towns.loc[node, 'rural settlement']
             level = data['level']
             population = towns.loc[node, 'population']
             popup_text = f"{name}<br>Размер: {level}<br>Население: {population}"
 
             color = level_colors[level]
-            radius = 7 if level == 'Второй уровень' else 5  # Больший радиус для уровня 2
+            radius = 7 if level == 'Сверхкрупный город' else 5  # Bigger radius for the largest city
 
             folium.CircleMarker(
                 location=[pos[1], pos[0]],
-                radius=radius,  # Стандартный радиус для всех узлов, можно настроить если нужно
+                radius=radius,
                 color=color,
                 fill=True,
                 fill_color=color,
                 fill_opacity=1,
-                popup=popup_text  # Упрощенный popup, можно дополнить информацией
+                popup=popup_text
             ).add_to(m)
 
-        # Создание легенды
-        legend_html = """
-            <div style="position: fixed; 
-                        bottom: 50px; left: 50px; width: 200px; height: 130px; 
-                        border:2px solid grey; z-index:9999; font-size:14px;
-                        background-color:white;
-                        ">
-            &nbsp; <b>Легенда</b> <br>
-            &nbsp; Пятый уровень &nbsp; <i class="fa fa-circle" style="font-size:20px;color:#6996B3"></i><br>
-            &nbsp; Четвертый уровень &nbsp; <i class="fa fa-circle" style="font-size:20px;color:#89AC76"></i><br>
-            &nbsp; Третий уровень &nbsp; <i class="fa fa-circle" style="font-size:20px;color:#FFC107"></i><br>
-            &nbsp; Второй уровень &nbsp; <i class="fa fa-circle" style="font-size:20px;color:#B71C1C"></i><br>
-            </div>
-            """
+        # Create legend
+        legend_html = '<div style="position: fixed; bottom: 50px; left: 50px; width: 300px; height: 350px; border:2px solid grey; z-index:9999; font-size:14px; background-color:white; padding: 10px;"><b>Легенда</b><br>'
+        for level, color in level_colors.items():
+            legend_html += f'&nbsp; {level} &nbsp; <i class="fa fa-circle" style="font-size:20px;color:{color}"></i><br>'
+        legend_html += '</div>'
 
-        # Добавление легенды на карту
+        # Add legend to the map
         m.get_root().html.add_child(folium.Element(legend_html))
 
-        # Сохранение карты в HTML-файл
+        # Save map to HTML file
         m.save(filepath)
 
     def save_graph_to_geojson(self, G: nx.Graph, filepath: str = None):
--- popframe/preprocessing/level_filler.py
+++ popframe/preprocessing/level_filler.py
@@ -8,52 +8,53 @@ from ..models.geodataframe import GeoDataFrame, BaseRow
 class TownRow(BaseRow):
     geometry: Point
     name: str
-    rural_settlement: str
+    population : int = Field(gt=0)
     level: str = Field(default="Нет уровня")
 
 
 class LevelFiller(BaseModel):
     towns: GeoDataFrame[TownRow]
-    administrative_centers: ClassVar[list[str]] = [
-        "Ломоносов",
-        "Тосно",
-        "Тихвин",
-        "Сосновый бор",
-        "Сланцы",
-        "Приозерск",
-        "Подпорожье",
-        "Луга",
-        "Лодейное Поле",
-        "Кировск",
-        "Кириши",
-        "Кингисепп",
-        "Гатчина",
-        "Выборг",
-        "Всеволожск",
-        "Волхов",
-        "Волосово",
-        "Бокситогорск"
-    ]
+    population_thresholds: ClassVar[dict[str, int]] = {
+        "Сверхкрупный город": 3000000,
+        "Крупнейший город": 1000000,
+        "Крупный город": 250000,
+        "Большой город": 100000,
+        "Средний город": 50000,
+        "Малый город": 50000,
+        "Крупное сельское поселение": 5000,
+        "Большое сельское поселение": 1000,
+        "Среднее сельское поселение": 200,
+        "Малое сельское поселение": 200,
+    }
 
     @staticmethod
     def _assign_level(row) -> str:
-        rural_settlement = row['rural_settlement'].strip()
-        name = row['name']
+        population = row['population']
         
-        if rural_settlement in ['деревня', 'село', 'хутор', 'местечко', 'кордон', 'деревня']:
-            return 'Пятый уровень'
-        elif rural_settlement in ['город', 'поселок', 'городской поселок', 'поселок при железнодорожной станции']:
-            return 'Четвертый уровень'
-        elif 'административный центр' in rural_settlement:
-            if name in LevelFiller.administrative_centers:
-                return 'Второй уровень'
-            return 'Третий уровень'
-        return 'Нет уровня'
+        if population > LevelFiller.population_thresholds["Сверхкрупный город"]:
+            return "Сверхкрупный город"
+        elif population > LevelFiller.population_thresholds["Крупнейший город"]:
+            return "Крупнейший город"
+        elif population > LevelFiller.population_thresholds["Крупный город"]:
+            return "Крупный город"
+        elif population > LevelFiller.population_thresholds["Большой город"]:
+            return "Большой город"
+        elif population > LevelFiller.population_thresholds["Средний город"]:
+            return "Средний город"
+        elif population > LevelFiller.population_thresholds["Крупное сельское поселение"]:
+            return "Малый город"
+        elif population > LevelFiller.population_thresholds["Большое сельское поселение"]:
+            return "Крупное сельское поселение"
+        elif population > LevelFiller.population_thresholds["Среднее сельское поселение"]:
+            return "Большое сельское поселение"
+        elif population > LevelFiller.population_thresholds["Малое сельское поселение"]:
+            return "Среднее сельское поселение"
+        else:
+            return "Малое сельское поселение"
 
     @field_validator("towns", mode="before")
     @classmethod
     def validate_towns(cls, gdf):
-        gdf.rename(columns={'rural settlement': 'rural_settlement'}, inplace=True)
         gdf["level"] = gdf.apply(cls._assign_level, axis=1)
         return GeoDataFrame[TownRow](gdf)
 
