c8896a6a816f33070279b32ddf8a4a93b657ff3a
Mvin8 <skitless1229@gmail.com>
2024-06-11 16:33:19 +0300

    small fix karkas

--- popframe/preprocessing/level_filler.py
+++ popframe/preprocessing/level_filler.py
@@ -14,43 +14,27 @@ class TownRow(BaseRow):
 
 class LevelFiller(BaseModel):
     towns: GeoDataFrame[TownRow]
-    population_thresholds: ClassVar[dict[str, int]] = {
-        "Сверхкрупный город": 3000000,
-        "Крупнейший город": 1000000,
-        "Крупный город": 250000,
-        "Большой город": 100000,
-        "Средний город": 50000,
-        "Малый город": 50000,
-        "Крупное сельское поселение": 5000,
-        "Большое сельское поселение": 1000,
-        "Среднее сельское поселение": 200,
-        "Малое сельское поселение": 200,
+    population_thresholds: ClassVar[dict[str, tuple[int, int]]] = {
+        "Сверхкрупный город": (3000000, float('inf')),
+        "Крупнейший город": (1000000, 3000000),
+        "Крупный город": (250000, 1000000),
+        "Большой город": (100000, 250000),
+        "Средний город": (50000, 100000),
+        "Малый город": (5000, 50000),
+        "Крупное сельское поселение": (3000, 5000),
+        "Большое сельское поселение": (1000, 3000),
+        "Среднее сельское поселение": (200, 1000),
+        "Малое сельское поселение": (0, 200),
     }
 
     @staticmethod
     def _assign_level(row) -> str:
         population = row['population']
         
-        if population > LevelFiller.population_thresholds["Сверхкрупный город"]:
-            return "Сверхкрупный город"
-        elif population > LevelFiller.population_thresholds["Крупнейший город"]:
-            return "Крупнейший город"
-        elif population > LevelFiller.population_thresholds["Крупный город"]:
-            return "Крупный город"
-        elif population > LevelFiller.population_thresholds["Большой город"]:
-            return "Большой город"
-        elif population > LevelFiller.population_thresholds["Средний город"]:
-            return "Средний город"
-        elif population > LevelFiller.population_thresholds["Крупное сельское поселение"]:
-            return "Малый город"
-        elif population > LevelFiller.population_thresholds["Большое сельское поселение"]:
-            return "Крупное сельское поселение"
-        elif population > LevelFiller.population_thresholds["Среднее сельское поселение"]:
-            return "Большое сельское поселение"
-        elif population > LevelFiller.population_thresholds["Малое сельское поселение"]:
-            return "Среднее сельское поселение"
-        else:
-            return "Малое сельское поселение"
+        for level, (lower_bound, upper_bound) in LevelFiller.population_thresholds.items():
+            if lower_bound < population <= upper_bound:
+                return level
+        return "Неизвестный уровень"  # На случай, если население не вписывается ни в один из диапазонов
 
     @field_validator("towns", mode="before")
     @classmethod
@@ -58,7 +42,7 @@ class LevelFiller(BaseModel):
         gdf["level"] = gdf.apply(cls._assign_level, axis=1)
         return GeoDataFrame[TownRow](gdf)
 
-    def fill_levels(self) -> gpd.GeoDataFrame:
+    def fill_levels(self) -> GeoDataFrame[TownRow]:
         towns = self.towns.copy()
         towns["level"] = towns.apply(self._assign_level, axis=1)
         return towns
